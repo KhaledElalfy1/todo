2cc842b25c6c5daabe5a9861d699a6b6700b44f1
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-03-14 09:23:32 +0200

    finish welcome screen

--- lib/core/routes/app_routers.dart
+++ lib/core/routes/app_routers.dart
@@ -4,6 +4,9 @@ import 'package:testfirebase/core/routes/routing.dart';
 import 'package:testfirebase/features/home/presentation/view/home.dart';
 import 'package:testfirebase/features/onboarding/presentation/controller/cubit/onboarding_cubit.dart';
 import 'package:testfirebase/features/onboarding/presentation/view/onboarding.dart';
+import 'package:testfirebase/features/sign_in/presentation/view/sign_in.dart';
+import 'package:testfirebase/features/sign_up/presentation/view/sign_up.dart';
+import 'package:testfirebase/features/welcome/presentation/view/welcome.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -13,13 +16,27 @@ class AppRouter {
         return MaterialPageRoute(
           builder: (_) => BlocProvider(
             create: (context) => OnboardingCubit(),
-            child:const Onboarding(),
+            child: const Onboarding(),
           ),
         );
       case Routing.home:
         return MaterialPageRoute(
           builder: (_) => const Home(),
         );
+      case Routing.welcomeScreen:
+        return MaterialPageRoute(
+          builder: (_) => const WelcomeScreen(),
+        );
+      case Routing.signUp:
+        return MaterialPageRoute(
+          builder: (_) => const SignUp(),
+        );
+
+      case Routing.signIn:
+        return MaterialPageRoute(
+          builder: (_) => const SignIn(),
+        );
+
       default:
         return MaterialPageRoute(
           builder: (_) => const Scaffold(
---
+++ lib/features/welcome/presentation/view/welcome.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:gap/gap.dart';
+import 'package:testfirebase/core/helpers/extentions.dart';
+import 'package:testfirebase/core/routes/routing.dart';
+import 'package:testfirebase/core/utils/app_color.dart';
+import 'package:testfirebase/core/utils/app_fonts.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0.w),
+          child: Column(
+            children: [
+              Gap(60.h),
+              Text(
+                'Welcome to UpTodo',
+                style: AppFonts.bold32White,
+              ),
+              Gap(20.h),
+              Text(
+                'Please login to your account or create new account to continue',
+                textAlign: TextAlign.center,
+                style: AppFonts.regular16White,
+              ),
+              const Spacer(),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.pushNamed(Routing.signIn);
+                  },
+                  child: Text(
+                    'LOGIN',
+                    style: AppFonts.regular16White,
+                  ),
+                ),
+              ),
+              Gap(20.h),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.pushNamed(Routing.signUp);
+                  },
+                  style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.transparent),
+                    side: MaterialStatePropertyAll(
+                      BorderSide(
+                        color: AppColor.primaryColor,
+                        width: 2,
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    'CREATE ACCOUNT',
+                    style: AppFonts.regular16White,
+                  ),
+                ),
+              ),
+              Gap(30.h),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
