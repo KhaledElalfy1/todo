bf5be2bab819bfebd1f615d86b608f86c20ef205
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-04-17 21:09:19 +0200

    Refactor initialization logic in main.dart and todo_app.dart
    Move initialization logic related to isFirstTime to main.dart and simplify
    getInitRoute method in todo_app.dart to use isFirstTime directly.

--- lib/features/onboarding/presentation/controller/cubit/onboarding_cubit.dart
+++ lib/features/onboarding/presentation/controller/cubit/onboarding_cubit.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:testfirebase/core/database/cache/shared_preferences.dart';
 import 'package:testfirebase/core/helpers/extentions.dart';
 import 'package:testfirebase/core/routes/routing.dart';
-import 'package:testfirebase/core/service/service_locator.dart';
 
 part 'onboarding_state.dart';
 
 class OnboardingCubit extends Cubit<OnboardingState> {
-  OnboardingCubit() : super(OnboardingInitial()) {
-    getIt<CacheHelper>().saveData(key: CacheKeys.isFirstTime, value: true);
-  }
+  OnboardingCubit() : super(OnboardingInitial());
   static OnboardingCubit get(context) => BlocProvider.of(context);
   int indicator = 0;
   String text = 'NEXT';
