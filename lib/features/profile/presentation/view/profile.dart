f2f1c88b4c6940cd9c468d0ca849b8641856767f
KhaledElalfy1 <khaledalfy53@gmail.com>
2024-05-02 15:58:55 +0300

    refactor profile file code

--- lib/features/profile/presentation/view/profile.dart
+++ lib/features/profile/presentation/view/profile.dart
@@ -1,14 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:testfirebase/core/utils/app_color.dart';
-import 'package:testfirebase/core/utils/app_icons.dart';
-import 'package:testfirebase/features/profile/presentation/controller/change_password_cubit/change_password_cubit.dart';
-import 'package:testfirebase/features/profile/presentation/controller/change_visibility_cubit/change_visibility_cubit.dart';
-import 'package:testfirebase/features/profile/presentation/view/widgets/change_password_dialog.dart';
-import 'package:testfirebase/features/profile/presentation/view/widgets/options_row.dart';
+import 'package:testfirebase/features/profile/presentation/view/widgets/change_password_section.dart';
 import 'package:testfirebase/features/profile/presentation/view/widgets/sign_out_section.dart';
 import 'package:testfirebase/features/profile/presentation/view/widgets/user_task_section.dart';
 import 'package:testfirebase/generated/l10n.dart';
@@ -42,29 +37,11 @@ class Profile extends StatelessWidget {
         Gap(25.h),
         const UserTasksSection(),
         Gap(15.h),
-        OptionSection(
-          iconPath: AppIcons.iconsKey,
-          title: S.of(context).changePassword,
-          onTap: () {
-            showDialog(
-              context: context,
-              builder: (context) => MultiBlocProvider(
-                providers: [
-                  BlocProvider(
-                    create: (context) => ChangePasswordCubit(),
-                  ),
-                  BlocProvider(
-                    create: (context) => ChangeVisibilityCubit(),
-                  )
-                ],
-                child: const ChangePasswordDialog(),
-              ),
-            );
-          },
-        ),
+      const  ChangePasswordSection(),
         Gap(30.h),
         const SignOutSection()
       ],
     );
   }
 }
+
