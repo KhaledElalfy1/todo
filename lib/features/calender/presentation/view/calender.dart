0e6288bbaa22d5fab28fed664660585746731dc3
khaled <khaledalfy53@gmail.com>
2024-06-15 12:53:38 +0300

    Refactor font sizes and add responsive font size calculation
    The commit refactors font sizes in `AppFonts` class and adds a function to calculate responsive font size based on the device's physical size and pixel ratio. The `Calender` widget is also updated to use a `CustomScrollView` instead of a `ListView`.

--- lib/features/calender/presentation/view/calender.dart
+++ lib/features/calender/presentation/view/calender.dart
@@ -8,7 +8,6 @@ import 'package:testfirebase/core/widgets/list_of_tasks.dart';
 import 'package:testfirebase/features/calender/presentation/controller/calender_cubit/calender_cubit.dart';
 import 'package:testfirebase/features/calender/presentation/controller/calender_cubit/calender_state.dart';
 
-
 import '../../../task_details/presentation/controller/cubit/edit_task_cubit.dart';
 import '../../../task_details/presentation/controller/cubit/edit_task_state.dart';
 
@@ -19,23 +18,25 @@ class Calender extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocBuilder<CalenderCubit, CalenderState>(
       builder: (context, state) {
-        return ListView(
-          children: [
-            Gap(10.h),
-            TableCalendar(
-              calendarStyle: const CalendarStyle(
-                isTodayHighlighted: false,
+        return CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(child: Gap(20.h),),
+            SliverToBoxAdapter(
+              child: TableCalendar(
+                calendarStyle: const CalendarStyle(
+                  isTodayHighlighted: false,
+                ),
+                headerStyle: const HeaderStyle(
+                    formatButtonVisible: false, titleCentered: true),
+                focusedDay: CalenderCubit.get(context).selectedDate,
+                firstDay: DateTime(2000),
+                lastDay: DateTime(2030),
+                onDaySelected: (selectedDay, focusedDay) {
+                  CalenderCubit.get(context).changeSelectedDay(selectedDay);
+                },
+                selectedDayPredicate: (day) =>
+                    isSameDay(day, CalenderCubit.get(context).selectedDate),
               ),
-              headerStyle: const HeaderStyle(
-                  formatButtonVisible: false, titleCentered: true),
-              focusedDay: CalenderCubit.get(context).selectedDate,
-              firstDay: DateTime(2000),
-              lastDay: DateTime(2030),
-              onDaySelected: (selectedDay, focusedDay) {
-                CalenderCubit.get(context).changeSelectedDay(selectedDay);
-              },
-              selectedDayPredicate: (day) =>
-                  isSameDay(day, CalenderCubit.get(context).selectedDate),
             ),
             BlocBuilder<EditTaskCubit, EditTaskState>(
               builder: (context, state) {
