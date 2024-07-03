97b57a433cd61fd6d2a31cc111d58e550ceee257
khaled <khaledalfy53@gmail.com>
2024-07-03 11:37:18 +0300

    add loading for changing image

--- lib/features/profile/presentation/view/profile.dart
+++ lib/features/profile/presentation/view/profile.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:gap/gap.dart';
 import 'package:testfirebase/core/widgets/cached_profile_photo.dart';
+import 'package:testfirebase/core/widgets/shimmer_custom_loading.dart';
 import 'package:testfirebase/features/profile/presentation/controller/update_name_cubit/update_name_cubit.dart';
 import 'package:testfirebase/features/profile/presentation/controller/update_name_cubit/update_name_state.dart';
 import 'package:testfirebase/features/profile/presentation/controller/update_new_image_cubit/update_new_image_cubit.dart';
@@ -35,11 +36,13 @@ class Profile extends StatelessWidget {
         BlocBuilder<UpdateNewImageCubit, UpdateNewImageState>(
           builder: (context, state) {
             return ClipOval(
-              child: CachedProfilePhoto(
-                hight: 100,
-                width: 100,
-                radius: 45.r,
-              ),
+              child: state is UpdateNewImageLoading
+                  ? const ShimmerCustomLoading(radius: 45)
+                  : CachedProfilePhoto(
+                      hight: 100,
+                      width: 100,
+                      radius: 45.r,
+                    ),
             );
           },
         ),
